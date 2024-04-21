580cf090005c71b064a91dfe0e15bbc5553bf5e5
user <evan.n.brown25@gmail.com>
2024-04-21 00:02:53 -0400

    Add header
    Added social links, imported material ui library, and added floating ship

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { FaDiscord, FaGithub } from "react-icons/fa";
+import airship from './airship.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          {/* Replace with another image later
-           <img src={logo} className="App-logo" alt="logo" /> */}
+      <div className="app">
+        <div className="app-header">
+          <div className="app-header__socials">
+            <a href="https://github.com/awesomebrownies" target="_blank" style={{margin: '50%'}}>
+                <FaGithub className="app-header__socials--button" style={{color: '#fafbfc', height: '3vw', width: '3vw'}}/>
+            </a>
+            <a href="https://discordapp.com/users/495612155677376534" target="_blank" style={{margin: '50%'}}>
+              <FaDiscord className="app-header__socials--button" style={{color: '#fafbfc', height: '3vw', width: '3vw'}} />
+            </a>
+          </div>
+          <img src={airship} className="app-airship" style={{height: '60%', width: '60%', margin: '3%'}}/>
         </div>
-        <p className="App-intro">
+        <p className="app-intro">
           Under construction.
         </p>
       </div>
