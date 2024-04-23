9214c1800cbf05df3613f68f82d743f7c7925122
user <evan.n.brown25@gmail.com>
2024-04-23 18:45:25 -0400

    Update Infiltration and ProjectMTF sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect, useRef, useCallback } from 'react';
 import './App.css';
+import ReactPlayer from 'react-player';
 import { FaDiscord, FaGithub } from "react-icons/fa";
 import ProjectMTFOutside from './images/project-mtf-outside.png';
 import ProjectMTFFacility from './images/project-mtf-facility.png';
@@ -10,9 +11,13 @@ import PaintedMap from './images/painted-map.png';
 import CodeFSH from './images/code-fsh.png';
 import CodeVSH from './images/code-vsh.png';
 import DamageVignette from './images/damage_vignette.png';
+import ProjectStructure from './images/project-structure.png';
+import ChatModes from './images/chat-modes.png';
 
 class App extends Component {
   render() {
+
+
     return (
       <div className="app">
         <div className="app-header">
@@ -43,17 +48,35 @@ class App extends Component {
             
               </ul>
             </div>
+          </div>
 
-            <img src={ProjectMTFFacility} style={{position: 'absolute', width: '26.6vw', height: '15vw', borderRadius: '5%', transform: 'translate(0%, 18vw)', zIndex: '0'}} />
-            <img src={ProjectMTFOutside} style={{position: 'absolute', width: '26.6vw', height: '15vw', borderRadius: '5%', transform: 'translate(35vw, 18vw)', zIndex: '0'}} />
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <img src={ProjectMTFFacility} style={{width: '26.6vw', height: '15vw', borderRadius: '5%'}} />
+            <img src={ProjectMTF} style={{width: '35vw', height: '20vw', borderRadius: '5%', overflow: 'hidden'}} />
+            <img src={ProjectMTFOutside} style={{width: '26.6vw', height: '15vw', borderRadius: '5%'}} />
+          </div>
 
-            <img src={ProjectMTF} style={{width: '35vw', height: '20vw', borderRadius: '5%', overflow: 'hidden', zIndex: '1'}} />
-            <img src={ProjectMTFAward} style={{width: '6vw', height: '6vw', transform: 'translate(-50%, 0)', zIndex: '2'}} />
-
-            <div style={{fontStyle: 'italic', color: 'gray', position: 'absolute', transform: 'translate(18vw, -15px)'}}>
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', color: '#BBBBBB', paddingLeft: '25vw', paddingRight: '25vw'}}>
+            <div style={{textAlign: 'center', fontStyle: 'italic', paddingBottom: '70px', color: 'gray'}}>
               "We die in the dark so you can live in the light"
             </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <img src={ProjectMTFAward} style={{width: '6vw', height: '6vw'}} />
+            </div>
+            <div>
+            During the hackathon event our team participated by creating a new gamemode from scratch, primarily developing during spring break.
+            We won in the overall category due to fantastic visuals, cutscenes, sound effects, and a squad based mission system with an intriguing storyline.
+            Unfortunately further visuals and videos cannot be produced since the project files have been lost.
+            </div>
+            <div style={{color: 'gray', paddingTop: '50px', textAlign: 'center'}}>
+                Skip to the timestamp 27:30 to watch the ProjectMTF section
+              </div>
+            <div style={{display: 'flex', justifyContent: 'center', alignContent: 'center', paddingTop: '5px'}}>
+              <ReactPlayer
+              url="https://www.youtube.com/live/O2N1xzvC5pM?si=-ruiQn1pEWx1T2Yh&t=1647" controls={true} />
+            </div>
           </div>
+
           <div className="app-intro__infiltration">
             <div style={{fontSize: '2vw', paddingBottom: '10px', letterSpacing: '2px'}}>INFILTRATION</div>
             <div style={{width: '50vw', paddingBottom: '50px', color: '#BBBBBB'}}> The game splits two teams into groups of squads, sub-divided into fireteams. Squad Leaders communicate with each other to coordinate attacks and hold positions in objectives. The game favors teamwork and will punish players who do not work together.</div>
@@ -70,13 +93,52 @@ class App extends Component {
             </div>
             <div style={{display: 'flex', flexDirection: 'row'}}>
               <div style ={{display: 'flex', flexDirection:'column'}}>
-                <img src={CodeVSH} style={{width: '26vw', height: '26vw', borderRadius: '5%', marginBottom: '1vw'}} />
-                <img src={CodeFSH} style={{width: '26.8vw', height: '20vw', borderRadius: '5%'}} />
+                <img src={CodeVSH} style={{width: '22.1vw', height: '22.1vw', borderRadius: '5%', marginBottom: '1vw'}} />
+                <img src={CodeFSH} style={{width: '25.5vw', height: '17vw', borderRadius: '5%'}} />
               </div>
               <div style={{width: '50vw', color: '#BBBBBB', padding: '10vw'}}>
                 Utilizing the OpenGL pipeline, it becomes possible to create all sorts of screen effects. Shown here is the setup for a health vignette, found in the files rendertype_text.vsh and rendertype_text.fsh.
                 <img src={DamageVignette} style={{width: '35vw', height: '20vw', marginTop: '1vw', borderRadius: '5%'}} />
               </div>
+            </div>
+            <div style={{display: 'flex', flexDirection: 'row', paddingTop: '30px'}}>
+              <ReactPlayer url="https://youtu.be/M3xHyf7THW4" controls={true} style={{paddingRight: '30px'}} />
+              <ReactPlayer url="https://youtu.be/uUrKWwJQe50" controls={true} />
+            </div>
+            <ul style={{paddingLeft: '25vw', paddingRight: '25vw', paddingTop: '30px', paddingBottom: '10px', color: '#BBBBBB'}}>
+              <li style={{paddingBottom: '10px'}}>
+                AABB Raycasting:
+                 To optimize each shot, the algorithm raycasts to all targets within view and tests for hits.
+                  If a raycast does hit, the algorithm initializes a BlockIterator from the player to the target while checking the bounding boxes of solid blocks for collisions.
+              </li>
+              <li style={{paddingBottom: '10px'}}>
+                Asynchronous Execution:
+                 Instead of waiting for the next tick to execute, the algorithm detects a block mining packet 
+                 -- Accurate left click detection is done by spawning client side empty sculk blocks at the players head --
+                  and immediately starts a repeating timer of 86ms if the gun is on automatic, allowing for faster feedback and a more fine tuned fire rate.
+              </li>
+              <li style={{paddingBottom: '10px'}}>
+                Lag Compensation:
+                 In an online environment, ping can result in hit invalidation, forcing players to lead their shots.
+                  To combat this, the gun system adds the player’s ping + the current target interpolation state, and rolls back the hitbox to the correct location of where the player saw the target when shooting.
+              </li>
+            </ul>
+
+            <div style={{display: 'flex', flexDirection: 'row', alignContent: 'center', justifyItems: 'center', paddingTop: '150px'}}>
+              <img src={ProjectStructure} style={{width: '10vw', height: '11.6vw', borderRadius: '5%', marginTop: '60px', marginRight: '10px'}}/>
+              <ReactPlayer url="https://youtu.be/k0wZm0jhGYg" controls={true} />   
+            </div>
+
+            <div style={{color: '#BBBBBB', paddingTop: '30px', paddingLeft: '25vw', paddingRight: '25vw'}}>
+            The menu is an object oriented system that allows for simplistic setups, direction based local coordinate conversions, and action customizability.
+            </div>
+
+            <img src={ChatModes} style={{width: '29.37 vw', height: '10vw', paddingTop: '150px'}} />
+            <div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', justifyItems: 'center', paddingTop: '30px', marginLeft: '25vw', marginRight: '25vw', color: '#BBBBBB'}}>
+            The system uses the Paper’s Adventure API for chat components.
+             To allow for more detailed rank symbols, each chat message uses a line for the player’s username, and another for the message.
+              The space to the left allows for up to a 14x14 pixel symbol.
+               Additionally, I create a web request to the site “insert site” and cache the resulting player head that is returned after using it in the message.
             </div>
           </div>
         </div>
